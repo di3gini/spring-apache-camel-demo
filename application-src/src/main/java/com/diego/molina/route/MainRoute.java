81da3be081bdbf85c7372512d5987fdc41602ac3
Diego Molina <di3gini3094@gmail.com>
2022-09-15 20:10:39 -0600

    Get random names from a public API

--- application-src/src/main/java/com/diego/molina/route/MainRoute.java
+++ application-src/src/main/java/com/diego/molina/route/MainRoute.java
@@ -16,7 +16,26 @@ public class MainRoute extends RouteBuilder{
 	
 	@Value("${finish-process.create-file}")
 	String createFile;
-	
+
+	//API URLS
+
+	@Value("${api.url.predict-gender}")
+	String predictGenderUrl;
+
+	@Value("${api.url.random-name}")
+	String randomNameUrl;
+
+	@Value("${api.url.predict-age}")
+	String predictAgeUrl;
+
+
+	private final static String DUMMY_URL        = "https://dummyurl?throwExceptionOnFailure=false";
+	private final static String JDBC_URL         = "jdbc:dataSource?useHeadersAsParameters=true";
+	private final static String DATETIME_NOW     = "${date:now:yyyy-MM-dd HH:mm:ss}";
+	private static final String CAMEL_HTTP_URI   = "CamelHttpUri";
+	private static final String CAMEL_HTTP_METHOD= "CamelHttpMethod";
+	private static final String IS_OK_HTTPCODE   = "${header.CamelHttpResponseCode} == 200";
+
 	@Override
 	public void configure() throws Exception {
 
@@ -26,10 +45,29 @@ public class MainRoute extends RouteBuilder{
 		.end();
 		
 		from(fromQuartz).routeId("quartzExecution").streamCaching()
-			.log("Apache camel demo is now running!")
-			.log("Log executed at ${date:now:yyyy-MM-dd HH:mm:ss}")
-			.to("direct:finishProcess")
+				.log("Apache camel demo is now running!")
+				.log("Log executed at ${date:now:yyyy-MM-dd HH:mm:ss}")
+				.to("direct:getNamesRoute")
+				.to("direct:finishProcess")
 		.end();		
+
+
+
+		from("direct:getNamesRoute").routeId("getNamesRoute")
+				.removeHeaders("*")
+				.log("Names url: " + randomNameUrl)
+				.setHeader(CAMEL_HTTP_METHOD, constant("GET"))
+				.setHeader(CAMEL_HTTP_URI, constant(randomNameUrl))
+				.to(DUMMY_URL)
+				.choice()
+					.when(simple(IS_OK_HTTPCODE))
+						.log("Got a 200 response from the server")
+						.log("Body got: ${body}")
+					.endChoice()
+					.otherwise()
+						.log("Error accesing API for names")
+				.end()
+		.end();
 
 		
 		/*
